bfd96e150419914dbf53d93b9bfd2b7bc7b8a237
MrAdmin23 <gibbs.jonothon@gmail.com>
2022-12-04 17:59:46 -0500

    FinD1D2

--- day1.py
+++ day1.py
@@ -1,28 +1,29 @@
+
 with open('day1.input') as file:
-    inputlist = [i for i in file.read().strip().split("\n")]
+    data = [i for i in file.read().strip().split("\n")]
 
-elf1 = 0
-elf2 = 0
-elf3 = 0
+
+elf = 0
+elf2 = 0    
+elf3 = 0    
 count = 0
-
-
-for food in inputlist:
-    if food =='':
-        count = 0 #skips to next elf
+for food in data:
+    if food == '':
+        count = 0      
     else:
-            num = int(food)
-            count += num
-
-    if count > elf1: 
+        num = int(food)
+        count += num   
+    if count > elf: 
         elf3 = elf2
-        elf2 = elf1
-        max = count
+        elf2 = elf
+        elf = count
     elif count > elf2:
         elf3 = elf2
-        elf2 = count    # Elf with second to most 
+        elf2 = count    # Elf with 2nd most 
     elif count > elf3:
-        elf3 = count    # Elf with third to most 
+        elf3 = count    # Elf with 3rd most 
 
-print("The Magic Number",elf1)
-print("The Magic Number for Day 2",elf1+elf2+elf3)
+
+# Answers
+print("Magic Number for Day 1:", elf)
+print("Magic Number for Day 2:", elf+elf2+elf3)
