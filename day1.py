bdd958b344331827c00c790cc0f2081b3cc4777a
MrAdmin23 <gibbs.jonothon@gmail.com>
2022-12-04 09:38:31 -0500

    Change2

--- day1.py
+++ day1.py
@@ -1,17 +1,17 @@
 with open('day1.input') as file:
     inputlist = [i for i in file.read().strip().split("\n")]
 
-
-max = 0
+elf1 = 0
+elf2 = 0
+elf3 = 0
+elf4 = 0
 count = 0
-for item in inputlist:
-    if item == "":
-        count = 0
+
+
+for food in inputlist:
+    if food =='':
+        count = 0 #skips to next elf
     else:
-        num = int(item)
-        count += num
+            num = int(food)
+            count += num
 
-if count > max:
-    max = count
-
-print ("D1",max)
