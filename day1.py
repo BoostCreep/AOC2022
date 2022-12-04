fc0c1388879b929e3e716f03147fd745bbd9a5ec
MrAdmin23 <gibbs.jonothon@gmail.com>
2022-12-04 09:51:30 -0500

    c3

--- day1.py
+++ day1.py
@@ -4,7 +4,6 @@ with open('day1.input') as file:
 elf1 = 0
 elf2 = 0
 elf3 = 0
-elf4 = 0
 count = 0
 
 
@@ -15,3 +14,15 @@ for food in inputlist:
             num = int(food)
             count += num
 
+    if count > elf1: 
+        elf3 = elf2
+        elf2 = elf1
+        max = count
+    elif count > elf2:
+        elf3 = elf2
+        elf2 = count    # Elf with second to most 
+    elif count > elf3:
+        elf3 = count    # Elf with third to most 
+
+print("The Magic Number",elf1)
+print("The Magic Number for Day 2",elf1+elf2+elf3)
